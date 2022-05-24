ccca5231fc2088c21eaa32fbad3c9d195ea8e588
Xyberr <makszacev238@gmail.com>
2022-05-24 19:38:50 +0500

    First commit

---
+++ Calc.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	float x;
+	
+	float y;
+	
+	x = 0;
+	
+	y = 0;
+	
+	char z;
+	
+	while(true) {
+
+		cin >> x >> y >> z;
+		
+		switch (z)
+		{
+
+		case '*':
+			cout << x * y;
+			break;
+
+		case '/':
+			cout << x / y;
+			break;
+
+		case '+':
+			cout << x + y;
+			break;
+
+		case '-':
+			cout << x - y;
+			break;
+
+		case '1':
+			return 0;
+
+		default:
+			return 0;
+
+		}
+	}
+
+	return 0;
+}
